a4a1a3f479eb6be29da0dcb6010ca06d0e069841
shanty <capilshanty@gmail.com>
2021-04-22 09:52:49 +0700

    Operator Unary

---
+++ src/operator/OperatorUnary.java
@@ -0,0 +1,23 @@
+package operator;
+
+public class OperatorUnary {
+    public static void main(String[] args) {
+        int i = 0;
+        i++;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        i=i+1;
+        System.out.println(i);
+
+        i+=3;
+        System.out.println(i);
+        i+=3;
+        System.out.println(i);
+
+        i*=3;
+        System.out.println(i);
+        i*=3;
+        System.out.println(i);
+    }
+}
