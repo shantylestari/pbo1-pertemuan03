fb25a9b5ca8da3c38f6aaa1a4cf28741f3c8038a
shanty <capilshanty@gmail.com>
2021-04-22 09:50:19 +0700

    Operator Binary

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class OperatorBinary {
+    String nama = "Shanty Lestari";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.11;
+
+    public static void main(String[] args) {
+
+            System.out.println(9+3);
+            System.out.println(9-3);
+            System.out.println(9*3);
+            System.out.println(9/3);
+            System.out.println(9%3);
+            System.out.println(9%2);
+    }
+}
