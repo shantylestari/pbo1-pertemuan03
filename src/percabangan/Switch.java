7215b303d86e2c3d7b399d5ae10da129cd54ea7a
shanty <capilshanty@gmail.com>
2021-04-22 10:28:51 +0700

    Switch 1

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch(jalanJalan){
+            case 1:
+                System.out.println("Urang lapas");
+                break;
+            case 2:
+                System.out.println("Urang acayan");
+                break;
+            case 3:
+                System.out.println("Ubat nyamuk");
+                break;
+            case 4:
+                System.out.println("dubil dit");
+                break;
+            case 5:
+                System.out.println("Makhluk Halus");
+                break;
+        }
+    }
+}
