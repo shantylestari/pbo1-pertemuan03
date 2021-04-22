73fe363f84f49c7af9dba79eb6e668912d9b7afe
shanty <capilshanty@gmail.com>
2021-04-22 10:27:30 +0700

    ifelse statement3

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,17 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDidompet = 50000;
+        double totalBelanja = 24000;
+        double uangDidompet = 25000;
 
-        if (uangDidompet < totalBelanja) {
+        if(uangDidompet<totalBelanja){
             System.out.println("Uang Kurang, kurangi jajan anda");
-        } else {
-            System.out.println("Uang cukup, berelaan lah");
+        }else if(uangDidompet>totalBelanja){
+            double angsul = uangDidompet-totalBelanja;
+            System.out.println("Uang cukup, angsul : "+angsul);
+        }else{
+            System.out.println("Uang pas... UwU....");
         }
+
     }
 }
